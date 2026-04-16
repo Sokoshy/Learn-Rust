1f70ba5ce19ee3860591dbbdbccd5fab384f0e88
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2026-04-16 13:57:56 +0200

    task Calculate Price

--- Common Programming Concepts/Variables, Functions, and Conditions Test/Calculate Price/src/lib.rs
+++ Common Programming Concepts/Variables, Functions, and Conditions Test/Calculate Price/src/lib.rs
@@ -1,3 +1,5 @@
-pub fn calculate_price(/* TODO */) /*TODO*/ {
-    /*write your solution here*/
+pub fn calculate_price(num_apple: i32 ) -> i32 {
+    if num_apple < 40 {
+        num_apple * 2
+    } else { num_apple }
 }
