0e483d243401d67254084222af089f774bd76f1d
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2026-05-15 10:05:32 +0200

    Struct, Methods, Enums, and Pattern Match: Structs with Methods

--- Structs, Methods, Enums, and Pattern Matching/Structs with Methods/Sending Packages/src/main.rs
+++ Structs, Methods, Enums, and Pattern Matching/Structs with Methods/Sending Packages/src/main.rs
@@ -8,18 +8,22 @@ struct Package {
 impl Package {
     fn new(sender_country: String, recipient_country: String, weight_in_grams: i32) -> Package {
         if weight_in_grams <= 0 {
-            // This shouldn't happen so we panic 
+            // This shouldn't happen so we panic
             panic!();
-        } 
-        /* Instantiate the package here */
+        }
+        Package {
+            sender_country,
+            recipient_country,
+            weight_in_grams,
+        }
     }
 
-    fn is_international(&self) -> /* Add return type */ {
-        /* Something goes here */
+    fn is_international(&self) -> bool {
+        self.sender_country != self.recipient_country
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> /* Add return type */ {
-        /* Something goes here */
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
+        cents_per_gram * self.weight_in_grams
     }
 }
 
