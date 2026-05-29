687fdb373736bb078d281028d16f587b8c747efd
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2026-05-29 12:56:31 +0200

    Common Collections: Strings

--- Common Collections/Strings/String Transformer/src/lib.rs
+++ Common Collections/Strings/String Transformer/src/lib.rs
@@ -8,9 +8,19 @@ pub mod transformer {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: /* Write type of input */) -> /* Write type of result */ {
+pub fn transformer(input: Vec<(&str, Command)>) -> Vec<String> {
         let mut output = vec![];
-        // TODO: Complete the function body!
+        for (string, command) in input {
+            match command {
+            Command::Uppercase => { output.push(string.to_uppercase()); },
+            Command::Trim => { output.push(string.trim().to_string()) }
+            Command::Append(n) => {
+                let bar= "bar".repeat(n);
+                let result = string.to_owned() + &bar;
+                output.push(result)
+                }
+            }
+        }
         output
     }
 }
