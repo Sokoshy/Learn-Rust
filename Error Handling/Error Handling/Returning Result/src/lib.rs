1730999e6b4b211c6e3013a1c99785e630d3ce69
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2026-06-11 11:56:30 +0200

    Error Handling: Error Handling

--- Error Handling/Error Handling/Returning Result/src/lib.rs
+++ Error Handling/Error Handling/Returning Result/src/lib.rs
@@ -1,8 +1,8 @@
-pub fn generate_nametag_text(name: String) -> Option<String> {
-    if name.len() > 0 {
-        Some(format!("Hi! My name is {}", name))
+pub fn generate_nametag_text(name: String) -> Result<String,String> {
+    if !name.is_empty() {
+        Ok(format!("Hi! My name is {}", name))
     } else {
         // The error message should be: "`name` was empty; it must be nonempty."
-        None
+        Err("`name` was empty; it must be nonempty.".to_string())
     }
 }
