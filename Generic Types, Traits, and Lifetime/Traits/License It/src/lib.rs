dbfddc9d869e079fd345385efa8e9f745631f22a
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2026-06-15 14:54:48 +0200

    Generic Types, Trait, and Lifetime: Traits

--- Generic Types, Traits, and Lifetime/Traits/License It/src/lib.rs
+++ Generic Types, Traits, and Lifetime/Traits/License It/src/lib.rs
@@ -1,5 +1,7 @@
 pub trait Licensed {
-    fn licensing_info(&self) -> String;
+    fn licensing_info(&self) -> String {
+        String::from("Some information")
+    }
 }
 
 pub struct SomeSoftware {
--- Generic Types, Traits, and Lifetime/Traits/Returning Types that Implement Traits/src/main.rs
+++ Generic Types, Traits, and Lifetime/Traits/Returning Types that Implement Traits/src/main.rs
@@ -29,4 +29,5 @@ fn returns_summarizable() -> impl Summary {
 
 fn main() {
     // put you code here to launch it
+    println!("{}", returns_summarizable().summarize());
 }
